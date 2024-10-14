cf41ee28319740bf20c79abd16683a5eaf4ad49a
Rajan-chaudhary-947 <chaudharyrajan947@gmail.com>
2024-10-15 02:57:45 +0530

    Add files via upload

---
+++ weatherApp/api/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.adarshelectronics.api
+
+object Constant {
+    val apiKey = "990dd8818eca4405a8090042242907"
+}
---
+++ weatherApp/api/WeatherModel.kt
@@ -0,0 +1,6 @@
+package com.example.adarshelectronics.api
+
+public final class WeatherModel(
+        val current: Current,
+        val location: Location
+    )
