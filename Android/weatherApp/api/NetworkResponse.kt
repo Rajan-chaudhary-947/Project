5057cea6cc35e2d371246107680cf271dd6d86d2
Rajan-chaudhary-947 <chaudharyrajan947@gmail.com>
2024-10-15 03:20:02 +0530

    Update NetworkResponse.kt

--- Android/weatherApp/api/NetworkResponse.kt
+++ Android/weatherApp/api/NetworkResponse.kt
@@ -1,4 +1,4 @@
-package com.example.adarshelectronics.api
+package com.Android.weatherApp.api
 
 sealed class NetworkResponse<out T>  {
     data class Success<out T>(val data: T) : NetworkResponse<T>()
