7dffba87d70e15fa0fb63e8c51c54b2eecf3cef6
Rajan-chaudhary-947 <chaudharyrajan947@gmail.com>
2024-10-15 03:18:41 +0530

    Update weatherPage.kt

--- Android/weatherApp/weatherPage.kt
+++ Android/weatherApp/weatherPage.kt
@@ -1,4 +1,4 @@
-package com.example.adarshelectronics
+package com.Android.weatherApp
 // Necessary Imports
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
